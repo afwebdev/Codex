332dc66a476b159011a247b925ce5c7b9e62fcf6
AFWebDev <andrewfariaj@gmail.com>
2019-11-09 23:37:40 -0500

    fix password functionality - encrypts now

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
-var bcrypt = require("bcryptjs");
 var mongoose = require("mongoose");
-
+var bcrypt = require("bcryptjs");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema(
@@ -16,12 +15,16 @@ var userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.plugin(require("mongoose-bcrypt"), { rounds: 8 });
+
 userSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
 };
 // Hooks are automatic methods that run during various phases of the User Model lifecycle
 // In this case, before a User is created, we will automatically hash their password
-userSchema.pre("save", () => {
+userSchema.pre("save", function() {
+  console.log(this.password);
+  console.log(docs);
   this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10), null);
 });
 
