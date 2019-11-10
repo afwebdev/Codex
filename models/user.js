ec1b732d8f435411c181d4bd8f9d1fcdeb76ddb6
AFWebDev <andrewfariaj@gmail.com>
2019-11-10 00:14:41 -0500

    fix user auths

--- models/user.js
+++ models/user.js
@@ -1,12 +1,13 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var mongoose = require("mongoose");
-var bcrypt = require("bcryptjs");
+var Bcrypt = require("bcryptjs");
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema(
   {
     email: {
-      type: String
+      type: String,
+      unique: true
     },
     password: {
       type: String
@@ -15,18 +16,18 @@ var userSchema = new Schema(
   { timestamps: true }
 );
 
-userSchema.plugin(require("mongoose-bcrypt"), { rounds: 8 });
-
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-};
-// Hooks are automatic methods that run during various phases of the User Model lifecycle
-// In this case, before a User is created, we will automatically hash their password
-userSchema.pre("save", function() {
-  console.log(this.password);
-  console.log(docs);
-  this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10), null);
+//This automatically adds a password field.
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = Bcrypt.hashSync(this.password, 10);
+  next();
 });
+
+userSchema.methods.comparePassword = function(plaintext, callback) {
+  return callback(null, Bcrypt.compareSync(plaintext, this.password));
+};
 
 var User = mongoose.model("User", userSchema);
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,19 +17,17 @@ router.post("/login", passport.authenticate("local"), function(req, res) {
 // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 // otherwise send back an error
-router.post("/signup", function(req, res) {
+router.post("/signup", function(req, resp) {
   console.log(req.body);
   db.User.create({
     email: req.body.email,
     password: req.body.password
   })
-    .then(function(newUser) {
-      console.log(newUser);
-      res.status(201).json(newUser);
+    .then(res => {
+      resp.send(res);
     })
-    .catch(function(err) {
-      console.log(err);
-      res.status(401).json(err);
+    .catch(err => {
+      resp.send(err.errmsg);
     });
 });
 
