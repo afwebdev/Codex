6b4797efad24802945788a51b83bcad4ee35508e
Manjyot Thandi <manjyot.thandi@gmail.com>
2019-11-23 13:47:50 -0500

    This is all changes on answer page

--- client/src/pages/Answer.js
+++ client/src/pages/Answer.js
@@ -2,98 +2,114 @@ import React, { Component, useContext, useState, useEffect } from "react";
 import withStyles from "@material-ui/styles/withStyles";
 import { withRouter } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
+// import Paper from "@material-ui/core/Paper";
+// import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
+// import Button from "@material-ui/core/Button";
+// import Avatar from "@material-ui/core/Avatar";
 import Topbar from "../components/Topbar";
-import Container from "@material-ui/core/Container";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Box from "@material-ui/core/Box";
+// import Container from "@material-ui/core/Container";
+// import Radio from "@material-ui/core/Radio";
+// import RadioGroup from "@material-ui/core/RadioGroup";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Box from "@material-ui/core/Box";
 import API from "../utils/API";
-import ToolTip from "@material-ui/core/Tooltip";
-import { Questionlist, Answerlist,Replylist } from "../components/QuestionAnswerList";
+// import ToolTip from "@material-ui/core/Tooltip";
+import {
+  Questionlist,
+  Answerlist,
+  Replylist
+} from "../components/QuestionAnswerList";
 import { LoginContext } from "./../components/LoginContext";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import { Reply } from "../components/Reply";
+import { makeStyles } from "@material-ui/styles";
+import Paper from "@material-ui/core/Paper";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-github";
 const backgroundShape = require("../images/Liquid-Cheese.svg");
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: "2em",
-    padding: theme.spacing(3, 2),
+    color: "red",
+    paddingTop: "50px",
     flexGrow: 1,
     overflow: "hidden",
     background: `url(${backgroundShape}) no-repeat`,
     backgroundSize: "cover",
-    backgroundPosition: "0 400px",
+    //backgroundPosition: "0 400px",
     paddingBottom: 200,
     height: "100vh"
   },
-  questionList: {
-    // color: "red"
+  question: {
+    textAlign: "center",
+    marginTop: "10px"
   },
-  avatar: {
-    fontSize: "14px",
-    fontWeight: "bold"
-  },
-  button: {
-    margin: theme.spacing(1)
+  paper: {
+    padding: theme.spacing(10),
+    margin: "auto",
+    maxWidth: 500
   }
-});
+}));
 
 const Answer = props => {
+  const classes = useStyles();
+  const currentPath = props.location.pathname;
   const [answerstate, setanswerstate] = useState({
     questions: [],
     answers: [],
-    reply: false,
-  })
+    reply: false
+  });
 
   // Once you click reply, this function will run
-  const reply = (event) => {
-    console.log('REPLY IS CLICKED STILL')
-    const replyId = event.target.id
-    setanswerstate((prevState) => ({
+  const reply = event => {
+    console.log("REPLY IS CLICKED STILL");
+    const replyId = event.target.id;
+    setanswerstate(prevState => ({
       ...prevState,
       reply: replyId
-    }))
-  }
-  
+    }));
+  };
+
   // API call to post a reply to an answer
-  const submitReply = (event) => {
-    const answer_id = event.target.id
-    console.log(answer_id)
-    const reply = document.getElementById(answer_id+1).value;
+  const submitReply = event => {
+    const answer_id = event.target.id;
+    console.log(answer_id);
+    const reply = document.getElementById(answer_id + 1).value;
     console.log(reply);
-    const comment = { 
+    const comment = {
       answer_id,
       comment: reply,
       rejection_reason: null,
       user_id: userStatus.userId
-    }
-    API.postReply(comment).then(res => {
-      setanswerstate(prevState => ({
-        ...prevState,
-        reply:false
-      }))
-      // This will allow you to get the refreshed comments, while updating state as well.
-      API.getQuestionAnswers(questionID).then(res => {
-        console.log('THIS IS ON COMPONENT MOUNT')
-        console.log(res.data.answer_id[0].comment_id);
-        setanswerstate({
-          questions: res.data.question_description,
-          answers: res.data.answer_id,
+    };
+    API.postReply(comment)
+      .then(res => {
+        setanswerstate(prevState => ({
+          ...prevState,
+          reply: false
+        }));
+        // This will allow you to get the refreshed comments, while updating state as well.
+        API.getQuestionAnswers(questionID).then(res => {
+          console.log("THIS IS ON COMPONENT MOUNT");
+          console.log(res.data.answer_id[0].comment_id);
+          setanswerstate({
+            questions: res.data.question_description,
+            answers: res.data.answer_id
+          });
+          console.log(res.data);
         });
-        console.log(res.data);
+        console.log("Posted the reply");
+      })
+      .catch(err => {
+        console.log(err);
       });
-      console.log("Posted the reply")
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
   };
 
   const [userStatus, setUserStatus] = useContext(LoginContext);
@@ -103,11 +119,11 @@ const Answer = props => {
     // sets state to the question and answers for that question
     console.log(questionID);
     API.getQuestionAnswers(questionID).then(res => {
-      console.log('THIS IS ON COMPONENT MOUNT')
+      console.log("THIS IS ON COMPONENT MOUNT");
       console.log(res.data.answer_id[0].comment_id);
       setanswerstate({
         questions: res.data.question_description,
-        answers: res.data.answer_id,
+        answers: res.data.answer_id
       });
       console.log(res.data);
     });
@@ -124,67 +140,97 @@ const Answer = props => {
       user_id: userStatus.userId
     };
     console.log(answerObj);
-    API.postAnswer(answerObj).then(res => {
-      setanswerstate(prevState => ({
-        ...prevState,
-        answers: res.data.answer_id
-      }))
-       // This will allow you to get the refreshed comments, while updating state as well.
-       API.getQuestionAnswers(questionID).then(res => {
-        console.log('THIS IS ON COMPONENT MOUNT')
-        console.log(res.data.answer_id[0].comment_id);
-        setanswerstate({
-          questions: res.data.question_description,
-          answers: res.data.answer_id,
+    API.postAnswer(answerObj)
+      .then(res => {
+        setanswerstate(prevState => ({
+          ...prevState,
+          answers: res.data.answer_id
+        }));
+        // This will allow you to get the refreshed comments, while updating state as well.
+        API.getQuestionAnswers(questionID).then(res => {
+          console.log("THIS IS ON COMPONENT MOUNT");
+          console.log(res.data.answer_id[0].comment_id);
+          setanswerstate({
+            questions: res.data.question_description,
+            answers: res.data.answer_id
+          });
+          console.log(res.data);
         });
-        console.log(res.data);
+        console.log("Posted the reply");
+      })
+      .catch(err => {
+        console.log(err);
       });
-      console.log("Posted the reply")
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
+  };
   let replyHTML;
   let buttonId;
 
-  
   return (
-    <div>
-      <Questionlist question={answerstate.questions} />
-      <li>
-        {answerstate.answers.map(answer => {
-          console.log(answer)
-          buttonId = answer._id
-          // setting the ID of the reply box
-          // This is for the conditional rendering of the reply box.
-          if(answerstate.reply === answer._id){
-             replyHTML = <Reply id={buttonId} submitReply={submitReply}/>
-          }
-          else{
-            replyHTML = <></>
-          }
-          return (
-            <>
-            <Answerlist id={answer._id} key={answer._id} reply={reply}>{answer.answer}</Answerlist>
-            {answer.comment_id.map(comment =>{
-              console.log(comment)
+    <>
+      <CssBaseline />
+      <Topbar currentPath={currentPath} />
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Questionlist
+            questionStyle={classes.question}
+            question={answerstate.questions}
+          />
+        </Paper>
+        <li>
+          <div
+            style={{
+              overflow: "scroll",
+              maxHeight: "500px",
+              maxWidth: "500px"
+            }}
+          >
+            {answerstate.answers.map(answer => {
+              console.log(answer);
+              buttonId = answer._id;
+              // setting the ID of the reply box
+              // This is for the conditional rendering of the reply box.
+              if (answerstate.reply === answer._id) {
+                replyHTML = <Reply id={buttonId} submitReply={submitReply} />;
+              } else {
+                replyHTML = <></>;
+              }
               return (
-              <Replylist>{comment.comment}</Replylist>
-              )
+                <>
+                  <Answerlist id={answer._id} key={answer._id} reply={reply}>
+                    {answer.answer}
+                  </Answerlist>
+                  {/* This is for collapsing the replies */}
+                  <ExpansionPanel>
+                    <ExpansionPanelSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <Typography className={classes.heading}>
+                        See All Replies
+                      </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                      <Typography>
+                        {answer.comment_id.map(comment => {
+                          console.log(comment);
+                          return <Replylist>{comment.comment}</Replylist>;
+                        })}
+                      </Typography>
+                    </ExpansionPanelDetails>
+                  </ExpansionPanel>
+                  {replyHTML}
+                </>
+              );
             })}
-            {replyHTML}
-            </>
-            )
-        })}
-      </li>
-      <textarea id="answertext" rows="4" cols="50">
-        HUH
-      </textarea>
-      <button onClick={submitAnswer}>Answer</button>
-    </div>
-   
+          </div>
+        </li>
+        <AceEditor mode="javascript" />
+        <textarea id="answertext" rows="4" cols="50"></textarea>
+        <button onClick={submitAnswer}>Answer</button>
+      </div>
+    </>
   );
 };
 
-export default withRouter(withStyles(styles)(Answer));
+export default withRouter(withStyles(useStyles)(Answer));
