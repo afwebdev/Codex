9bba535ff723be570a766c9274e4f1b2c0a74c8f
jatsunltd <jatsunltd@gmail.com>
2019-11-27 16:41:33 -0500

    code cleanup

--- client/src/components/PayPal.js
+++ client/src/components/PayPal.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { PayPalButton } from "react-paypal-button-v2";
- 
+
 export default class PayPal extends Component {
   render() {
     return (
@@ -9,7 +9,7 @@ export default class PayPal extends Component {
         // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
         onSuccess={(details, data) => {
           alert("Transaction completed by " + details.payer.name.given_name);
- 
+
           // OPTIONAL: Call your server to save the transaction
           return fetch("/paypal-transaction-complete", {
             method: "post",
--- client/src/pages/Payments.js
+++ client/src/pages/Payments.js
@@ -3,26 +3,26 @@ import React from "react";
 import { withRouter, useHistory } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 // import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
+// import Typography from "@material-ui/core/Typography";
+// import Grid from "@material-ui/core/Grid";
+// import Button from "@material-ui/core/Button";
+// import Card from "@material-ui/core/Card";
 // import CardActions from '@material-ui/core/CardActions';
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import Grow from "@material-ui/core/Grow";
 // import Slide from '@material-ui/core/Slide';
 import Topbar from "../components/Topbar";
-import ScrollyScroll from "../components/ScrollyScroll";
-import TestimonialAvatar from "../components/TestimonialAvatar";
+// import ScrollyScroll from "../components/ScrollyScroll";
+// import TestimonialAvatar from "../components/TestimonialAvatar";
 import Footer from "../components/Footer";
-import Quote from "../components/Quote";
-import logo from "../images/pngLogo.png";
-import Box from "@material-ui/core/Box";
-import Rating from "@material-ui/lab/Rating";
-import PayPalButton from "../components/PayPal"
+// import Quote from "../components/Quote";
+// import logo from "../images/pngLogo.png";
+// import Box from "@material-ui/core/Box";
+// import Rating from "@material-ui/lab/Rating";
+import PayPalButton from "../components/PayPal";
 
 const backgroundShape = require("../images/Liquid-Cheese.svg");
 
@@ -90,28 +90,6 @@ function Main(props) {
     return history.push("/dashboard");
   };
 
-  // const cards = [
-  //   {
-  //     name: "Vladimir P.",
-  //     quote:
-  //       "To forgive the terrorists is up to God, but to send them to him is up to me.",
-  //     stars: 4,
-  //     url: "http://www.hotavatars.com/wp-content/uploads/2019/01/I80W1Q0.png"
-  //   },
-  //   {
-  //     name: "Anonymous",
-  //     quote: "I finessed my way into a dev role thanks to Codex",
-  //     stars: 5,
-  //     url: "https://www.nuclearinst.com/write/MediaUploads/avataaars.png"
-  //   },
-  //   {
-  //     name: "Anonymous",
-  //     quote: "Told me I cannot build my whole app using HTML... Liers",
-  //     stars: 2,
-  //     url: "https://goticradio.files.wordpress.com/2017/11/avataaars.png?w=264"
-  //   }
-  // ];
-
   return (
     <React.Fragment>
       <CssBaseline />
@@ -120,22 +98,7 @@ function Main(props) {
         <div className={classes.root}>
           <main>
             <div className={classes.headerContent}>
-              <Container className={classes.container} maxWidth="sm">
-                {/* <img
-                  className={classes.logo}
-                  src={logo}
-                  alt="you-had-one-job"
-                ></img>
-                <Typography
-                  variant="h5"
-                  align="center"
-                  color="textSecondary"
-                  paragraph
-                >
-                  We strive to cultivate an environment that empower developers
-                  and connect them to solutions that enable productivity,
-                  growth, and discovery. jjjjjjjjjjjjjkkkkkkkkgvvvvvvvvvv
-                </Typography> */}
+              <Container className={classes.container} maxWidth="md">
                 <PayPalButton></PayPalButton>
               </Container>
             </div>
@@ -143,44 +106,9 @@ function Main(props) {
               className={classes.cardGrid}
               maxWidth="md"
               id="to-the-reviews"
-            >
-              {/* <Grid container spacing={4}>
-                {cards.map((card, index) => (
-                  <Grid item key={index} xs={12} sm={6} md={4}>
-                    <Card className={classes.card}>
-                      <CardMedia>
-                        <TestimonialAvatar url={card.url} />
-                      </CardMedia>
-                      <CardContent className={classes.cardContent}>
-                        <Box display="flex" flexDirection="row">
-                          <Box p={1} flexGrow={1}>
-                            <Typography
-                              gutterBottom
-                              variant="h5"
-                              component="h2"
-                            >
-                              {card.name}
-                            </Typography>
-                          </Box>
-                          <Box p={1}>
-                            <Rating
-                              name="read-only"
-                              value={card.stars}
-                              readOnly
-                            />
-                          </Box>
-                        </Box>
-                        <Quote quote={card.quote} />
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid> */}
-            </Container>
+            ></Container>
           </main>
-          <div>
-            
-          </div>
+          <div></div>
         </div>
       </Grow>
       <Footer />
